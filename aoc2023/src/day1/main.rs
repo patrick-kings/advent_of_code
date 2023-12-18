e67e23798e79fd68ebc3e884e9e50a8c229cbaf2
kings <patrick.kings@live.com>
2023-12-18 22:13:20 +0300

    2023 day1

--- aoc2023/src/day1/main.rs
+++ aoc2023/src/day1/main.rs
@@ -1,7 +1,17 @@
+use regex::Regex;
+
 fn main() {
     read_calibration_data();
 }
 fn read_calibration_data() {
+    let re = match Regex::new(r"(\d{1})") {
+        Ok(reg) => reg,
+        Err(err) => {
+            println!("couldn't compile regex, error is: {}", err);
+            return;
+        }
+    };
+
     let contents = match std::fs::read_to_string("aoc2023/src/day1/calibration.txt") {
         Ok(lines) => lines,
         Err(err) => {
@@ -10,7 +20,25 @@ fn read_calibration_data() {
         }
     };
 
+    let mut total: u32 = 0;
+
     for line in contents.lines() {
-        println!("{}", line)
+        let rs = match re.captures(line) {
+            Some(rs) => rs,
+            None => continue,
+        };
+
+        let reversed = line.chars().rev().collect::<String>();
+
+        let rs2 = match re.captures(reversed.as_str()) {
+            Some(rs) => rs,
+            None => continue,
+        };
+
+        let num = format!("{}{}", &rs[0], &rs2[0]).parse::<u32>().unwrap();
+
+        total = num + total;
     }
+
+    println!("Total is {}", total)
 }
