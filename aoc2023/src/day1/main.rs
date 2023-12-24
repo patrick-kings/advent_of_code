543d17aff7e96b3093f3fccb31afdfbc5e68da08
kings <patrick.kings@live.com>
2023-12-24 11:37:24 +0300

    day 1 part 2 - not working

--- aoc2023/src/day1/main.rs
+++ aoc2023/src/day1/main.rs
@@ -9,10 +9,75 @@ fn main() {
         }
     };
 
-    get_total(contents);
+    part1(contents.clone());
+
+    part2(contents);
 }
 
-fn get_total(contents: String) {
+fn part2(contents: String) {
+    let regx = match Regex::new(r"(?i)(\d{1}|one|two|three|four|five|six|seven|eight|nine)") {
+        Ok(regx_set) => regx_set,
+        Err(err) => panic!("error while compiling the regex : {}", err),
+    };
+    let mut total: u32 = 0;
+
+    let mut count: u16 = 0;
+    for line in contents.lines() {
+        let mut caps = regx.captures_iter(line);
+
+        let first = caps
+            .next()
+            .map_or("", |l| l.get(0).map_or("", |l1| l1.as_str()));
+
+        let last = caps
+            .last()
+            .map_or("", |l| l.get(0).map_or("", |l1| l1.as_str()));
+
+        let parse_text = |num_text: &str| -> u8 {
+            return match num_text {
+                "zero" => 0,
+                "one" => 1,
+                "two" => 2,
+                "three" => 3,
+                "four" => 4,
+                "five" => 5,
+                "six" => 6,
+                "seven" => 7,
+                "eight" => 8,
+                "nine" => 9,
+                _ => 0,
+            };
+        };
+
+        let fst: u8 = match first.parse::<u8>() {
+            Ok(rs) => rs,
+            Err(_) => parse_text(first),
+        };
+
+        let lst: u8 = if last.is_empty() {
+            fst
+        } else {
+            match last.parse::<u8>() {
+                Ok(rs) => rs,
+                Err(_) => parse_text(last),
+            }
+        };
+
+        let num = format!("{}{}", fst, lst).parse::<u32>().unwrap();
+
+        total = num + total;
+
+        count = count + 1;
+
+        if count == 2 {
+            println!("line {}:  {}", count, num);
+        }
+    }
+
+    println!("new total is {}", total);
+}
+
+fn part1(contents: String) {
     let re = match Regex::new(r"(\d{1})") {
         Ok(reg) => reg,
         Err(err) => {
